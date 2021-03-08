cfa2c8ad4946cc6b851210368100cc96df38af1e
radiumfi <79784488+radiumfi@users.noreply.github.com>
2021-03-08 18:13:19 +0800

    rebrand to radium finance

--- src/components/Menu/config.ts
+++ src/components/Menu/config.ts
@@ -12,11 +12,11 @@ const config: MenuEntry[] = [
     items: [
       {
         label: 'Exchange',
-        href: 'https://exchange.goosedefi.com/',
+        href: '#',
       },
       {
         label: 'Liquidity',
-        href: 'https://exchange.goosedefi.com/#/pool',
+        href: '#',
       },
     ],
   },
@@ -26,9 +26,9 @@ const config: MenuEntry[] = [
     href: '/farms',
   },
   {
-    label: 'Nests',
+    label: 'Stake',
     icon: 'PoolIcon',
-    href: '/nests',
+    href: '/stakes',
   },
   // {
   //   label: 'Pools',
@@ -45,27 +45,27 @@ const config: MenuEntry[] = [
   //   icon: 'NftIcon',
   //   href: '/nft',
   // },
-  {
-    label: 'Info',
-    icon: 'InfoIcon',
-    items: [
-      {
-        label: 'PancakeSwap',
-        href: 'https://pancakeswap.info/token/0xF952Fc3ca7325Cc27D15885d37117676d25BfdA6',
-      },
-      {
-        label: 'CoinGecko',
-        href: 'https://www.coingecko.com/en/coins/goose-finance',
-      },
-      {
-        label: 'CoinMarketCap',
-        href: 'https://coinmarketcap.com/currencies/goose-finance/',
-      },
-      {
-        label: 'AstroTools',
-        href: 'https://app.astrotools.io/pancake-pair-explorer/0x19e7cbecdd23a16dfa5573df54d98f7caae03019',
-      },
-    ],
+  // {
+  //  label: 'Info',
+  //  icon: 'InfoIcon',
+  //  items: [
+  //    {
+  //      label: 'PancakeSwap',
+  //      href: 'https://pancakeswap.info/token/',
+  //    },
+  //    {
+  //      label: 'CoinGecko',
+  //      href: 'https://www.coingecko.com/en/coins/',
+  //    },
+  //    {
+  //      label: 'CoinMarketCap',
+  //      href: 'https://coinmarketcap.com/currencies//',
+  //    },
+  //    {
+  //      label: 'AstroTools',
+   //     href: 'https://app.astrotools.io/pancake-pair-explorer/',
+  //    },
+  //  ],
   },
   {
     label: 'More',
@@ -73,32 +73,17 @@ const config: MenuEntry[] = [
     items: [
       {
         label: 'Github',
-        href: 'https://github.com/goosedefi/',
+        href: 'https://github.com/radiumfi/',
       },
       {
         label: 'Docs',
-        href: 'https://goosedefi.gitbook.io/goose-finance/',
+        href: '#',
       },
       {
         label: 'Blog',
-        href: 'https://goosefinance.medium.com/',
+        href: 'https://radiumfinance.medium.com/',
       },
     ],
-  },
-  {
-    label: 'Partnerships/IFO',
-    icon: 'GooseIcon',
-    href: 'https://docs.google.com/forms/d/e/1FAIpQLSe7ycrw8Dq4C5Vjc9WNlRtTxEhFDB1Ny6jlAByZ2Y6qBo7SKg/viewform?usp=sf_link',
-  },
-  {
-    label: 'Audit by Hacken',
-    icon: 'AuditIcon',
-    href: 'https://www.goosedefi.com/files/hackenAudit.pdf',
-  },
-  {
-    label: 'Audit by CertiK',
-    icon: 'AuditIcon',
-    href: 'https://certik.org/projects/goose-finance',
   },
 ]
 
--- src/views/Home/components/TwitterCard.tsx
+++ src/views/Home/components/TwitterCard.tsx
@@ -35,7 +35,7 @@ const TwitterCard = () => {
         <Timeline
           dataSource={{
             sourceType: 'profile',
-            screenName: 'GooseFinance'
+            screenName: 'RadiumFinance'
           }}
           options={{
             height: '300',
